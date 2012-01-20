338e0f589466398a7606cf8aaf5835eb685a6462
Cole Diamond <cd1am0nd@yahoo.com>
2012-01-19 23:20:12 -0500

    asdf

--- settings.py
+++ settings.py
@@ -164,15 +164,6 @@ TEMPLATE_DIRS = (
 	TEMPLATE_PATH,
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-"django.contrib.auth.context_processors.auth",
-"django.core.context_processors.debug",
-"django.core.context_processors.i18n",
-"django.core.context_processors.media",
-"django.core.context_processors.static",
-"django.contrib.messages.context_processors.messages",
-"views.default_context"
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
